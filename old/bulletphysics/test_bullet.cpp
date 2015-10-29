9d8e75fc76997e560f65d051c7d5e487758337a7
Julien Dehos <dehos@lisic.univ-littoral.fr>
2015-10-29 16:52:21 +0100

    test bullet with static allocation

--- old/bulletphysics/test_bullet.cpp
+++ old/bulletphysics/test_bullet.cpp
@@ -1,76 +1,57 @@
 
-// g++ -O2 -std=c++14 `pkg-config --cflags --libs bullet` test_bullet.cpp 
+// g++ -O2 -g -std=c++11 `pkg-config --cflags --libs bullet` test_bullet.cpp
+// valgrind --leak-check=full --show-leak-kinds=all ./a.out
 
 #include <iostream>
-#include <memory>
-
 #include <btBulletDynamicsCommon.h>
 
 int main (void)
 {
+    btDbvtBroadphase broadphase;
 
-    std::unique_ptr<btBroadphaseInterface> broadphase 
-        = std::make_unique<btDbvtBroadphase>();
+    btDefaultCollisionConfiguration collisionConfiguration;
 
-    std::unique_ptr<btDefaultCollisionConfiguration> collisionConfiguration 
-        = std::make_unique<btDefaultCollisionConfiguration>();
+    btCollisionDispatcher dispatcher(&collisionConfiguration);
 
-    std::unique_ptr<btCollisionDispatcher> dispatcher 
-        = std::make_unique<btCollisionDispatcher>(collisionConfiguration.get());
+    btSequentialImpulseConstraintSolver solver;
 
-    std::unique_ptr<btSequentialImpulseConstraintSolver> solver 
-        = std::make_unique<btSequentialImpulseConstraintSolver>();
+    btDiscreteDynamicsWorld dynamicsWorld (&dispatcher, &broadphase, &solver, 
+            &collisionConfiguration);
 
-    std::unique_ptr<btDiscreteDynamicsWorld> dynamicsWorld 
-        = std::make_unique<btDiscreteDynamicsWorld>(dispatcher.get(),
-                broadphase.get(), solver.get(), collisionConfiguration.get());
+    dynamicsWorld.setGravity(btVector3(0, -10, 0));
 
-    dynamicsWorld->setGravity(btVector3(0, -10, 0));
+    btStaticPlaneShape groundShape(btVector3(0, 1, 0), 1);
 
-    std::unique_ptr<btCollisionShape> groundShape 
-        = std::make_unique<btStaticPlaneShape>(btVector3(0, 1, 0), 1);
+    btSphereShape fallShape(1);
 
-    std::unique_ptr<btCollisionShape> fallShape 
-        = std::make_unique<btSphereShape>(1);
-
-    std::unique_ptr<btDefaultMotionState> groundMotionState 
-        = std::make_unique<btDefaultMotionState>(
-                btTransform(btQuaternion(0, 0, 0, 1), btVector3(0, -1, 0)));
+    btDefaultMotionState groundMotionState (
+            btTransform(btQuaternion(0, 0, 0, 1), btVector3(0, -1, 0)));
 
     btRigidBody::btRigidBodyConstructionInfo groundRigidBodyCI(
-            0, groundMotionState.get(), groundShape.get(), btVector3(0, 0, 0));
+            0, &groundMotionState, &groundShape, btVector3(0, 0, 0));
 
-    std::unique_ptr<btRigidBody> groundRigidBody 
-        = std::make_unique<btRigidBody>(groundRigidBodyCI);
+    btRigidBody groundRigidBody (groundRigidBodyCI);
 
-    dynamicsWorld->addRigidBody(groundRigidBody.get());
+    dynamicsWorld.addRigidBody(&groundRigidBody);
 
-
-    std::unique_ptr<btDefaultMotionState> fallMotionState 
-        = std::make_unique<btDefaultMotionState>(
-                btTransform(btQuaternion(0, 0, 0, 1), btVector3(0, 50, 0)));
+    btDefaultMotionState fallMotionState (
+            btTransform(btQuaternion(0, 0, 0, 1), btVector3(0, 50, 0)));
 
     btScalar mass = 1;
     btVector3 fallInertia(0, 0, 0);
-    fallShape->calculateLocalInertia(mass, fallInertia);
+    fallShape.calculateLocalInertia(mass, fallInertia);
     btRigidBody::btRigidBodyConstructionInfo fallRigidBodyCI(mass, 
-            fallMotionState.get(), fallShape.get(), fallInertia);
-    std::unique_ptr<btRigidBody> fallRigidBody 
-        = std::make_unique<btRigidBody>(fallRigidBodyCI);
-    dynamicsWorld->addRigidBody(fallRigidBody.get());
+            &fallMotionState, &fallShape, fallInertia);
 
+    btRigidBody fallRigidBody (fallRigidBodyCI);
+    dynamicsWorld.addRigidBody(&fallRigidBody);
 
-    for (int i = 0; i < 300; i++) {
-        dynamicsWorld->stepSimulation(1 / 60.f, 10);
-
+    for (int i = 0; i < 50; i++) {
+        dynamicsWorld.stepSimulation(1 / 10.f, 10);
         btTransform trans;
-        fallRigidBody->getMotionState()->getWorldTransform(trans);
-
-        std::cout << "sphere height: " << trans.getOrigin().getY() << std::endl;
+        fallRigidBody.getMotionState()->getWorldTransform(trans);
+        std::clog << "sphere height: " << trans.getOrigin().getY() << std::endl;
     }
-
-    dynamicsWorld->removeRigidBody(fallRigidBody.get());
-    dynamicsWorld->removeRigidBody(groundRigidBody.get());
 
     return 0;
 }
