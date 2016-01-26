b5ac6eec087b9178bc1353416d8d732156244368
Julien Dehos <dehos@lisic.univ-littoral.fr>
2016-01-26 02:19:23 +0100

    add a test executable

---
+++ src/petanqulator_test/petanqulator_test.cpp
@@ -0,0 +1,14 @@
+// Copyright © 2015 Dehos <dehos@lisic.univ-littoral.fr>
+// This work is free. You can redistribute it and/or modify it under the
+// terms of the Do What The Fuck You Want To Public License, Version 2,
+// as published by Sam Hocevar. See the COPYING.WTFPL file for more details.
+
+#include <petanqulator/Game.hpp>
+
+int main()
+{
+    // TODO petanqulator_test
+
+    return 0;
+}
+
