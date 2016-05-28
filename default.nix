a3f31478cc634232d685720d331bfd025daccd73
Julien Dehos <dehos@lisic.univ-littoral.fr>
2016-05-28 14:33:33 +0200

    nixos config

--- default.nix
+++ default.nix
@@ -2,7 +2,7 @@ with import <nixpkgs> {};
 
 stdenv.mkDerivation {
     name = "petanqulator";
-    buildInputs = [ stdenv cmake bullet pkgconfig mesa_glu gtkmm ];
+    buildInputs = [ stdenv cmake bullet xorg.libXmu pkgconfig mesa_glu gnome.gtkglextmm gnome.gtkmm xorg.libxcb xorg.libpthreadstubs ];
     src = ./.;
 }
 
