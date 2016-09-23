196388451ce5f5b1cc8f436c0807525c02508eea
Julien Dehos <dehos@lisic.univ-littoral.fr>
2016-09-23 14:28:18 +0200

    nix + tup

--- default.nix
+++ default.nix
@@ -2,7 +2,21 @@ with import <nixpkgs> {};
 
 stdenv.mkDerivation {
     name = "petanqulator";
-    buildInputs = [ stdenv cmake bullet xorg.libXmu pkgconfig mesa_glu gnome.gtkglextmm gnome.gtkmm xorg.libxcb xorg.libpthreadstubs ];
+    buildInputs = [ stdenv tup bullet xorg.libXmu pkgconfig mesa_glu gnome.gtkglextmm gnome.gtkmm xorg.libxcb xorg.libpthreadstubs ];
     src = ./.;
+    configurePhase = ''
+        mkdir -p $out/build_tup/petanqulator
+        mkdir -p $out/build_tup/petanqulator_cli
+        mkdir -p $out/build_tup/petanqulator_gui
+        substituteInPlace Tupfile --replace "build_tup" "$out/build_tup"
+        tup generate script.sh
+    '';
+    buildPhase = ''
+        ./script.sh
+    '';
+    installPhase = ''
+        mkdir -p $out/bin
+        cp $out/build_tup/*.out $out/bin/
+   '';
 }
 
